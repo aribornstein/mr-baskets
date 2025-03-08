e2346c3a39942a362c5e2482da67ae3ffd374810
aribornstein <x@x.com>
2025-03-08 22:31:35 +0200

    Refactor import paths in main.js to use relative paths

--- src/main.js
+++ src/main.js
@@ -9,10 +9,10 @@ import { handleSurfaceAdded } from "./managers/surfaceManager.js";
 import { state } from "./managers/stateManager.js";
 import { RealityAccelerator } from "ratk";
 import { ScoreboardManager } from "./gameplay/scoreboardManager.js";
-import { createBallAndHoop, removeBallAndHoop, moveHoopToNewPosition } from "../managers/spawnManager.js";
+import { createBallAndHoop, removeBallAndHoop, moveHoopToNewPosition } from "./managers/spawnManager.js";
 import { registerBallInput, updateBall } from "./gameplay/ballManager.js";
 import { isBasket } from "./gameplay/hoopManager.js";
-import { playBackgroundMusic, stopBackgroundMusic, loadBounceSound, playBounceSound } from "../effects/audioManager.js";
+import { playBackgroundMusic, stopBackgroundMusic, loadBounceSound, playBounceSound } from "./effects/audioManager.js";
 
 let clockGame, accumulator = 0, fixedTimeStep = 1 / 60;
 let ratk;
